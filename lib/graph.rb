38bbf75ccada81367837c00ee2206705679a9407
michdsouza <michd2005@gmail.com>
2016-05-16 17:00:26 -0300

    Refactored tests

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,55 +1,69 @@
 class Graph
-  attr_accessor :nodes, :matrix
+  class IndexError < StandardError; end
+  attr_accessor :libraries, :matrix
 
-	def initialize()
-    @nodes = []
+
+	def initialize
+    @libraries = []
 		@matrix = []
 	end
 
+  def add(library)
+    libraries << library
+    matrix << []
+  end
+
+  def find_index(library)
+    libraries.index(library)
+  end
+
+  def dependency?(library_index)
+    matrix.map { |a| a[library_index] == true }.any?
+  end
+
+  def remove(library)
+    library_index = find_index(library)
+    raise IndexError.new('This library cannot be removed') unless can_be_removed?(library_index)
+    remove_from_matrix(library_index)
+    remove_from_libraries(library)
+  end
+
   def add_from_command(library, dependencies=[])
-    library_index = find_node(library)
+    library_index = find_index(library)
     return true if library_index
-    return false if !dependencies.empty? && !(dependencies - nodes).empty?
-    library_index = add(library)
+    return false if !dependencies.empty? && !(dependencies - libraries).empty?
+    add(library)
+    library_index = find_index(library)
     add_dependencies(library_index, dependencies) 
     true
   end
 
-  def add(node)
-    nodes << node
-    matrix << []
-    # Return node index
-    nodes.length - 1 
-  end
-
-  def find_node(node)
-    nodes.index(node)
-  end
-
   def add_dependencies(library_index, dependencies)
     dependencies.each do |dependency|
-      dependency_index = find_node(dependency)
+      dependency_index = find_index(dependency)
       matrix[library_index][dependency_index] = true
     end
   end
 
-  def remove(library)
-    return if dependency?(library)
-    library_index = find_node(library)
-    # Remove column from matrix
-    matrix.map {|row| row.slice!(library_index) }
-    # Remove row from matrix
-    matrix.slice!(library_index)
-    # Remove from nodes
-    nodes.delete(library)
+  private
+
+  def can_be_removed?(library_index)
+    !library_index.nil? && !dependency?(library_index)
   end
 
-  def dependency?(library)
-    library_index = find_node(library)
-    library_index.nil? ? false : matrix.map { |a| a[library_index] == true }.any?
+  def remove_from_libraries(library)
+    libraries.delete(library)
   end
+
+  # Removes column from matrix
+  # Removes row from matrix
+  def remove_from_matrix(library_index)
+    matrix.map {|row| row.slice!(library_index) }
+    matrix.slice!(library_index)
+  end
+
+  
+
+ 
 
 end
-
-# matrix[*][col] == true is list of libraries needing col dependency
-# matrix[row][*] == true is libraries list of dependencies
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -3,17 +3,100 @@ require 'graph.rb'
 describe Graph do
   let(:graph) { Graph.new }
 
+  describe '#add' do
+    before(:each) do
+      graph.add('actionmailer')
+    end
+    
+    it 'adds a new library to the list of libraries' do
+      expect(graph.libraries).to eq ['actionmailer']
+    end
+    
+    it 'adds a new library to the adjacency matrix' do
+      expect(graph.matrix).to eq [[]]
+    end
+  end
+
+  describe '#find_index' do
+    before(:each) do
+      graph.add('actionmailer')
+    end
+    
+    context 'when library found' do
+      it 'returns the index of the library' do
+        expect(graph.find_index('actionmailer')).to eq 0
+      end
+    end
+    
+    context 'when library not found' do
+      it 'returns nils' do
+        expect(graph.find_index('ruby')).to eq nil
+      end
+    end
+  end
+
+  describe '#dependency?' do
+    before(:each) do
+      graph.libraries = ['mail', 'actionmailer']
+      graph.matrix = [[], [true]]
+    end
+
+    context 'when library is a dependency' do
+      it 'returns true' do
+        expect(graph.dependency?(0)).to eq true
+      end
+    end
+
+    context 'when library is not a dependency' do
+      it 'returns false' do
+        expect(graph.dependency?(1)).to eq false
+      end
+    end
+  end
+
+  describe '#remove' do
+    before(:each) do
+      graph.libraries = ['mail', 'actionmailer', 'ruby']
+      graph.matrix = [[true], [true], [nil, nil, true]]
+    end
+
+    context 'when library can be removed' do
+      it 'removes the library from the matrix' do
+        graph.remove('actionmailer')
+        expect(graph.matrix).to match_array([[true], [nil, true]])
+      end
+
+      it 'removes the library from libraries' do
+        graph.remove('actionmailer')
+        expect(graph.libraries).to match_array(['mail', 'ruby'])
+      end 
+    end
+
+    context 'when library cannot be removed' do
+      it 'raises an error' do
+        expect { graph.remove('something') }.to raise_error Graph::IndexError
+      end 
+    end
+  end
+  
+
+
+
+
+
+
+
 	describe '#add_node' do
     it 'adds a new library' do
       graph.add_from_command('actionmailer')
-      expect(graph.nodes).to eq ['actionmailer']
+      expect(graph.libraries).to eq ['actionmailer']
       expect(graph.matrix).to eq [[]]
     end
 
     context 'mail does not exist' do
       it 'does not add library' do
         expect(graph.add_from_command('actionmailer', ['mail'])).to eq false
-        expect(graph.nodes).to eq []
+        expect(graph.libraries).to eq []
       end
     end
 
@@ -24,7 +107,7 @@ describe Graph do
 
       it 'adds a library' do
         expect(graph.add_from_command('actionmailer', ['mail'])).to eq true
-        expect(graph.nodes).to match_array(['actionmailer', 'mail'])
+        expect(graph.libraries).to match_array(['actionmailer', 'mail'])
         expect(graph.matrix).to match_array([[],[true]])
       end
 
@@ -38,38 +121,10 @@ describe Graph do
 
       it 'adds a library' do
         expect(graph.add_from_command('actionmailer', ['mail', 'ruby'])).to eq true
-        expect(graph.nodes).to match_array(['actionmailer', 'mail', 'ruby'])
+        expect(graph.libraries).to match_array(['actionmailer', 'mail', 'ruby'])
         expect(graph.matrix).to match_array([[], [], [true, true]])
       end
     end
 	end
-
-  describe '#dependency?' do
-    before(:each) do
-      graph.add_from_command('mail')
-      graph.add_from_command('actionmailer', ['mail'])
-    end
-
-    it 'checks if node can be removed' do
-      expect(graph.dependency?('mail')).to eq true
-      expect(graph.dependency?('actionmailer')).to eq false
-    end
-  end
-
-  describe '#remove_node' do
-    before(:each) do
-      graph.nodes = ['mail', 'actionmailer', 'ruby']
-      graph.matrix = [[true], [true], [nil, nil, true]]
-    end
-
-    it 'removes the node from the matrix' do
-      graph.remove('actionmailer')
-      expect(graph.matrix).to match_array([[true], [nil, true]])
-    end
-
-    it 'removes the node from nodes' do
-      graph.remove('actionmailer')
-      expect(graph.nodes).to match_array(['mail', 'ruby'])
-    end 
-  end
+  
 end
