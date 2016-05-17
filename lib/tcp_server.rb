e4d0e0a8d33b6eb012b23a8d6d804ff3aaf66f1f
michdsouza <michd2005@gmail.com>
2016-05-17 20:25:18 -0300

    Added processing for concurrency 100

--- lib/tcp_server.rb
+++ lib/tcp_server.rb
@@ -2,27 +2,20 @@ require 'socket'
 require 'thread'
 require './lib/input_processor.rb'
 
-server = TCPServer.new(8080)
 processor = InputProcessor.new
 mutex = Mutex.new
 
-loop do
-  Thread.start(server.accept) do |socket|
-    while line = socket.gets
-      mutex.synchronize do  
-        socket.print processor.process(line)
+Socket.tcp_server_loop(8080) {|socket, _|
+  Thread.new {
+    begin
+      while line = socket.gets
+        mutex.synchronize do  
+          socket.print processor.process(line)
+        end
       end
     end
-  end
-end
-
-# loop do
-#   socket = server.accept
-#   while line = socket.gets
-#     response = processor.process(line)
-#     socket.print response
-#   end
-# end
+  }
+}
 
 # TODO: Refactor & write tests
 # Concurrency = 100
